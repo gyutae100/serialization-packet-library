7f1ec48a15cd7e527b945b2da4e90270ac3773ea
gyutae100 <gyutae100@gitgub.com>
2019-01-16 18:03:38 +0900

    no message

---
+++ CPacket/CPacket.h
@@ -0,0 +1,173 @@
+
+#ifndef  __PACKET__
+#define  __PACKET__
+
+#include "stdafx.h"
+
+class CPacket
+{
+public:
+
+	/*---------------------------------------------------------------
+	Packet Enum.
+
+	----------------------------------------------------------------*/
+	enum en_PACKET
+	{
+		eBUFFER_DEFAULT = 1024		// 패킷의 기본 버퍼 사이즈.
+	};
+
+	//////////////////////////////////////////////////////////////////////////
+	// 생성자, 파괴자.
+	//
+	// Return:
+	//////////////////////////////////////////////////////////////////////////
+	CPacket();
+	CPacket(int iBufferSize);
+
+	virtual	~CPacket();
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 패킷  파괴.
+	//
+	// Parameters: 없음.
+	// Return: 없음.
+	//////////////////////////////////////////////////////////////////////////
+	void	Destroy(void);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 패킷 청소.
+	//
+	// Parameters: 없음.
+	// Return: 없음.
+	//////////////////////////////////////////////////////////////////////////
+	void	Clear(void);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 사이즈 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (int)패킷 버퍼 사이즈 얻기.
+	//////////////////////////////////////////////////////////////////////////
+	int	GetBufferSize(void);
+	//////////////////////////////////////////////////////////////////////////
+	// 현재 사용중인 사이즈 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (int)사용중인 데이타 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		GetDataSize(void);
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 포인터 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (char *)버퍼 포인터.
+	//////////////////////////////////////////////////////////////////////////
+	char	*GetBufferPtr(void);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 Pos 이동. (음수이동은 안됨)
+	// GetBufferPtr 함수를 이용하여 외부에서 강제로 버퍼 내용을 수정할 경우 사용. 
+	//
+	// Parameters: (int) 이동 사이즈.
+	// Return: (int) 이동된 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		MoveIdxRear(int _idx_offset);
+	int		MoveIdxFront(int _idx_offset);
+
+
+
+
+
+
+	/* ============================================================================= */
+	// 연산자 오퍼레이터.
+	/* ============================================================================= */
+	CPacket	&operator = (CPacket &clSrcPacket);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 넣기.	각 변수 타입마다 모두 만듬.
+	//////////////////////////////////////////////////////////////////////////
+	CPacket	&operator << (BYTE byValue);
+	CPacket	&operator << (char chValue);
+
+	CPacket	&operator << (short shValue);
+	CPacket	&operator << (WORD wValue);
+
+	CPacket	&operator << (int iValue);
+	CPacket	&operator << (DWORD dwValue);
+	CPacket	&operator << (float fValue);
+
+	CPacket	&operator << (__int64 iValue);
+	CPacket	&operator << (double dValue);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 빼기.	각 변수 타입마다 모두 만듬.
+	//////////////////////////////////////////////////////////////////////////
+	CPacket	&operator >> (BYTE &byValue);
+	CPacket	&operator >> (char &chValue);
+
+	CPacket	&operator >> (short &shValue);
+	CPacket	&operator >> (WORD &wValue);
+
+	CPacket	&operator >> (int &iValue);
+	CPacket	&operator >> (DWORD &dwValue);
+	CPacket	&operator >> (float &fValue);
+
+	CPacket	&operator >> (__int64 &iValue);
+	CPacket	&operator >> (double &dValue);
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 데이타 얻기.
+	//
+	// Parameters: (char *)Dest 포인터. (int)Size.
+	// Return: (int)복사한 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		GetData(char *dest_buffer, int _idx_offset);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 데이타 삽입.
+	//
+	// Parameters: (char *)Src 포인터. (int)SrcSize.
+	// Return: (int)복사한 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		PutData(char *src_buffer, int sz_byte_src);
+
+
+
+
+protected:
+
+	char *_queue_buffer;
+
+	int _idx_front;
+	int _idx_rear;
+
+	int _total_sz_byte_buffer;
+	int _total_sz_byte_data;
+
+	union TYPE_FLOAT
+	{
+		char data[4];
+		float value;
+	};
+
+	union TYPE_DOUBLE
+	{
+		char data[8];
+		double value;
+	};
+
+};
+
+#endif
---
+++ CPacket/main.h
@@ -0,0 +1,16 @@
+
+#include "stdafx.h"
+#include "CPacket.h"
+
+
+void main() {
+
+
+
+
+	return;
+
+	
+
+
+}
